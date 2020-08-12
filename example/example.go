232408700ed901bafffff4b62e8b3cd58f8f059d
wanghongwei2 <wanghongwei2@100tal.com>
2020-08-12 12:05:37 +0800

    增加字符串匹配

--- example/example.go
+++ example/example.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"fmt"
 	engine2 "github.com/abusizhishen/expressMatchEngine/src/engine"
-	"log"
 )
 
 func main() {
+	// int match
 	engine,err := engine2.New("[(age>{1}|0)]")
 	if err != nil{
 		panic(err)
 	}
-	log.Println(engine.Match(map[string]string{"age":"1"}))
+	fmt.Println(engine.Match(map[string]string{"age":"1"}))
 	// output false,nil
 
-	log.Println(engine.Match(map[string]string{"age":"2"}))
+	fmt.Println(engine.Match(map[string]string{"age":"2"}))
 	// output true,nil
 
+	//string match
+	engine,err = engine2.New("[(word={hello}|3)]")
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(engine.Match(map[string]string{"word":"hell"}))
+	// output false,nil
+
+	fmt.Println(engine.Match(map[string]string{"word":"hello"}))
+	// output true,nil
 }
